5b7d7db844abd4599f6abf5bd093a91df751b3e3
ismailofficial <fb832.01@gmail.com>
2021-11-21 01:26:02 +0700

    ISMAIL

---
+++ plugins/donasi.js
@@ -0,0 +1,12 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
+┌〔 Donasi • Pulsa 〕
+├ XL Axis *083805279906*
+├ Indosat *085723180552*
+└────
+`.trim(), '© IsmailKurama', 'Donasi', '.donasi', m)
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
